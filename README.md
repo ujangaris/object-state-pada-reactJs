fb736020296f0dee04e131a6eac4ba012aba9886
ujangaja <ujangaja@gmail.com>
2023-02-06 15:50:51 +0700

    setState pada functin matikan() & memasang kondisi

--- README.md
+++ README.md
@@ -17,3 +17,13 @@
             buat state dan setState untuk menampilkan mengubah status,
             serta gunakan  spread operator agar data lamanya tidak tertimpa dengan setState
         2. pada browser ketika tombol nyalakan di klik , status akan jd 'ON' dan bensin akan berkurang 5L jadi 15L
+
+## setState pada functin matikan() & memasang kondisi
+
+    Todo:
+        1. Object.js
+            - merubah status 'ON'  menjadi 'OFF'  memasang function matikan()
+              yang didalamnya terdapat setState yang terhubung dengan tombol button
+              ketika diklik akan membuat status menjadi 'OFF
+            - kondisi jika bensi 0, maka akan tampil pesan peringatan
+            - disable untuk tiap button dengan kondisi
--- src/Object.js
+++ src/Object.js
@@ -9,16 +9,34 @@ const Object = () => {
     harga: 10000000,
     plat: "B 1234 ABC",
     status: "OFF",
+    peringatan: "-",
     // method
     nyalakan: () => {
-      console.log("nyalakan mesin")
       //   jalankan setState untuk merubah data
       setMtr((data) => {
+        if (data.bensin <= 0) {
+          return {
+            ...data,
+            status: "OFF",
+            peringatan: "Bensin Habis!",
+          }
+        } else {
+          console.log("nyalakan mesin")
+          return {
+            // spread operator
+            ...data,
+            status: "ON",
+            bensin: data.bensin - 5,
+          }
+        }
+      })
+    },
+    matikan: () => {
+      console.log("Matikan Mesin")
+      setMtr((data) => {
         return {
-          // spread operator
           ...data,
-          status: "ON",
-          bensin: data.bensin - 5,
+          status: "OFF",
         }
       })
     },
@@ -46,9 +64,15 @@ const Object = () => {
         <li> Merk: {mtr.merk} </li> <li> Tipe: {mtr.tipe} </li>{" "}
         <li> Warna: {mtr.warna} </li> <li> Bensin: {mtr.bensin}L </li>{" "}
         <li> Harga: {mtr.harga} </li> <li> Plat: {mtr.plat} </li>{" "}
-        <li> Status: {mtr.status} </li>{" "}
+        <li> Status: {mtr.status} </li> <li> pringatan: {mtr.peringatan} </li>{" "}
       </ul>{" "}
-      <button onClick={() => mtr.nyalakan()}> Nyalakan Mesin </button>{" "}
+      <button onClick={() => mtr.nyalakan()} disabled={mtr.status == "ON"}>
+        {" "}
+        Nyalakan Mesin{" "}
+      </button>{" "}
+      <button onClick={mtr.matikan} disabled={mtr.status == "OFF"}>
+        Matikan
+      </button>
     </div>
   )
 }
